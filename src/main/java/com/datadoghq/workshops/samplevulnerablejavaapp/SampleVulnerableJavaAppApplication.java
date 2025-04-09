a9b578c00ef5344530e8598658bac87bf8e99a9f
englieh <englieh@users.noreply.github.com>
2025-04-09 21:22:40 +0800

    Update SampleVulnerableJavaAppApplication.java

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/SampleVulnerableJavaAppApplication.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/SampleVulnerableJavaAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleVulnerableJavaAppApplication {
+public class SampleVulnerableJavaAppApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleVulnerableJavaAppApplication.class, args);
